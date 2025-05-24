059a5acd23e28f3dbaea6fa0bb50df755d5c6d5d
peterdinis <petter1@pobox.sk>
2025-05-24 12:07:16 +0200

    FOO

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import DashboardWrapper from "@/components/dashboard/DashboardWrapper";
+import { NextPage } from "next";
+
+const DashobardPage: NextPage = () => {
+    return <DashboardWrapper />
+}
+
+export default DashobardPage;
