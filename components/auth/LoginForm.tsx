d03ff6eed9bc4202f7dd53fc6b896548bbc5b517
peterdinis <petter1@pobox.sk>
2025-05-24 14:01:31 +0200

    feat: Start working on auth WIP

---
+++ components/auth/LoginForm.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const LoginForm: FC = () => {
+    return (
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm">
+                Login Form
+            </div>
+        </div>
+    )
+}
+
+export default LoginForm
---
+++ components/auth/RegisterForm.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const RegisterForm: FC = () => {
+    return (
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm">
+                Register
+            </div>
+        </div>
+    )
+}
+
+export default RegisterForm
